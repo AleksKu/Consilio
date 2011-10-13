9b5823575a595ce4387fc7d845c17192317a54e6
Алексей Кузнецов <mrakobesov@gmail.com>
2011-10-13 20:01:37 +0400

    CronBundle

---
+++ src/Aristos/CronBundle/CronBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Aristos\CronBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+
+class CronBundle extends Bundle
+{
+
+}
